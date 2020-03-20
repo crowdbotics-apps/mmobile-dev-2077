96daa09680ef03b9bf3e7fb822b50a6e34bf5f37
Crowdbotics <team@crowdbotics.com>
2020-03-20 07:25:36 +0000

    add package blueprint-react-native-emailauth-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import EmailAuth663Navigator from '../features/EmailAuth663/navigator';
 import UserProfile662Navigator from '../features/UserProfile662/navigator';
 import EmailAuthNavigator from '../features/EmailAuth/navigator';
 
@@ -17,6 +18,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+EmailAuth663: { screen: EmailAuth663Navigator },
 UserProfile662: { screen: UserProfile662Navigator },
 EmailAuth: { screen: EmailAuthNavigator },
 
--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -6,6 +6,7 @@ import { combineReducers } from "redux";
 
 
 //@BlueprintReduxImportInsertion
+import EmailAuth663Reducer from '../features/EmailAuth663/redux/reducers';
 import EmailAuthReducer from '../features/EmailAuth/redux/reducers';
 
 export const combinedReducers = combineReducers({
@@ -16,6 +17,7 @@ export const combinedReducers = combineReducers({
 
 
   //@BlueprintReduxCombineInsertion
+EmailAuth663: EmailAuth663Reducer,
 EmailAuth: EmailAuthReducer,
 
 });
